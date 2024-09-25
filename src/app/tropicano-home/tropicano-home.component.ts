1e4ec4187c50f2d5aa4c5b152ee557a3d6e8f068
Difroy <unifroy@gmail.com>
2024-09-25 12:16:40 +0200

    Update

--- src/app/tropicano-home/tropicano-home.component.ts
+++ src/app/tropicano-home/tropicano-home.component.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { ListTropicanosComponent } from '../list-tropicanos/list-tropicanos.component';
 import { TropicanoStatsComponent } from "../tropicano-stats/tropicano-stats.component";
 
-interface EducationLevel
-{
-  none:number,
-  hs:number,
-  college:number
-}
 
 @Component({
   selector: 'app-tropicano-home',
