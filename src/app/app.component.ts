237f82a8e8aece0ce70fd1a61e54c96a996d7fcd
Difroy <unifroy@gmail.com>
2024-09-25 12:42:10 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { ListTropicanosComponent } from './list-tropicanos/list-tropicanos.compo
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TropicanoComponent, TropicanoComponent, TropicanoHomeComponent],
+  imports: [RouterOutlet, TropicanoComponent, TropicanoHomeComponent, ListTropicanosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
