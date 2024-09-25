6048d28a01f272a7de41425eee2ca56b469c7d0d
Difroy <unifroy@gmail.com>
2024-09-25 12:18:35 +0200

    Update

--- src/app/tropicano-stats/tropicano-stats.component.ts
+++ src/app/tropicano-stats/tropicano-stats.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TropicanoService } from '../tropicano.service';
+import { Tropicano } from '../model/Tropicano';
+
+
+
+
+interface EducationLevel
+{
+    none:number,
+    hs:number,
+    college:number
+}
+
+
 
 @Component({
   selector: 'app-tropicano-stats',
@@ -10,9 +24,41 @@ import { Component } from '@angular/core';
 export class TropicanoStatsComponent {
 
 
+  tropicanos:Tropicano[] = [];
+
+  ngOnInit():void
+  {
+      this
+        .tropicanoService
+        .getTropicanos()
+        .subscribe(tropicanos=>this.tropicanos = tropicanos);
+  }
+
+  constructor(private tropicanoService:TropicanoService){}
+
+  education():EducationLevel
+  {
+      let res:EducationLevel = {none:0, hs:0, college:0};
+
+      if(!this.tropicanos)
+        return res;
+
+      for(let i=0;i<this.tropicanos.length;i++)
+        switch(this.tropicanos[i].education)
+        {
+            case "NONE":
+              res.none++;
+            break;
+            case "HS":
+              res.hs++;
+            break;
+            default:
+              res.college++;
+        }
+        return res;
+  }
 
 
 
-  
 
 }
--- src/app/tropicano.service.ts
+++ src/app/tropicano.service.ts
@@ -10,7 +10,7 @@ export class TropicanoService {
 
   constructor(private http:HttpClient) {}
 
-APIURL: string = 'http://localhost:8080/tropico/api/tropicanos';
+APIURL:string = 'http://localhost:8080/tropico/api/tropicanos';
 
 
 
