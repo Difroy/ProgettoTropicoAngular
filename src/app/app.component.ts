f36bf3148f3ed62ff06987c4f2477aad73183978
Difroy <unifroy@gmail.com>
2024-09-25 10:42:54 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TropicanoComponent } from "./tropicano/tropicano.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TropicanoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
