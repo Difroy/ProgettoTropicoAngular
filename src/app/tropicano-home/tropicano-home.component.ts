e9a5f57290e82e1f6c60a539ce4f822518b34d3b
Difroy <unifroy@gmail.com>
2024-09-25 12:15:46 +0200

    Update tropicano-home.component.ts

--- src/app/tropicano-home/tropicano-home.component.ts
+++ src/app/tropicano-home/tropicano-home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { TropicanoService } from '../tropicano.service';
-import { Tropicano } from '../model/Tropicano';
+import { Component } from '@angular/core';
+import { ListTropicanosComponent } from '../list-tropicanos/list-tropicanos.component';
+import { TropicanoStatsComponent } from "../tropicano-stats/tropicano-stats.component";
 
 interface EducationLevel
 {
@@ -12,59 +12,11 @@ interface EducationLevel
 @Component({
   selector: 'app-tropicano-home',
   standalone: true,
-  imports: [],
+  imports: [ListTropicanosComponent, TropicanoStatsComponent],
   templateUrl: './tropicano-home.component.html',
   styleUrl: './tropicano-home.component.css'
 })
 export class TropicanoHomeComponent {
-
-
-  tropicanos:Tropicano[]= [];
-
-ngOnInit(): void{
-
-this.tropicanoService
-.getTropicanos()
-.subscribe(tropicanos=>this.tropicanos=tropicanos)
-}
-
-
-constructor(private tropicanoService: TropicanoService)
-{}
-
-
-
-
-
-getEducationMap():EducationLevel
-{
-
-
-let res: EducationLevel = {none:0, hs:0, college:0};
-
-
-if(!this.tropicanos)
-  return res;
-
-for (let i=0; i<this.tropicanos.length;i++)
-  switch(this.tropicanos[i].education)
-{
-case "NONE":
-  res.none++;
-  break;
-case "HS":
-  res.hs++;
-  break;
-case "COLLEGE":
-  res.college++;
-  break;
-
-
-}
-
-
-}
-
 
 
 }
