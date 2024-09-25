dd239fa5ac44b1ca2a32eaf8e9bcfd9ca9466c92
Difroy <unifroy@gmail.com>
2024-09-25 15:26:09 +0200

    Update form

--- src/app/tropicano-form/tropicano-form.component.ts
+++ src/app/tropicano-form/tropicano-form.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Tropicano } from '../model/Tropicano';
+import { FormsModule } from '@angular/forms';
+import { TropicanoService } from '../tropicano.service';
 
 @Component({
   selector: 'app-tropicano-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './tropicano-form.component.html',
   styleUrl: './tropicano-form.component.css'
 })
 export class TropicanoFormComponent {
+
+  constructor(private tropicanoService:TropicanoService){}
+
+  tropicano:Tropicano=
+  {
+      id:0,
+      name:"",
+      surname:"",
+      birthyear:2000,
+      gender:"M",
+      education:"NONE",
+      party:"LEFT",
+      satisfaction:0
+  }
+
+  save():void
+  {
+      let copy:Tropicano = {...this.tropicano};
+      this.tropicanoService.saveTropicano(copy);
+
+      this.tropicano =  {
+        id:0,
+        name:"",
+        surname:"",
+        birthyear:2000,
+        gender:"M",
+        education:"NONE",
+        party:"LEFT",
+        satisfaction:0
+    };
+  }
+
+
+
+
+
 
 }
