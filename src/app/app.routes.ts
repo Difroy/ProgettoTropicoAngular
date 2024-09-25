7eb78f91adf1f1ac0603209bc1eb5f592fed6084
Difroy <unifroy@gmail.com>
2024-09-25 15:21:20 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TropicanoHomeComponent } from './tropicano-home/tropicano-home.component';
+import { BuildingHomeComponent } from './building-home/building-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    { path: '', component: TropicanoHomeComponent }, 
+    { path: 'tropicanos', component: TropicanoHomeComponent },
+    { path: 'buildings', component: BuildingHomeComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route for 404
+
+
+];
