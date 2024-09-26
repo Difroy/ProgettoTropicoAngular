619517b95576911c9af99831cac7b4387a55c472
Difroy <unifroy@gmail.com>
2024-09-26 16:48:14 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { TropicanoComponent } from "./tropicano/tropicano.component";
 import { TropicanoHomeComponent } from "./tropicano-home/tropicano-home.component";
 import { ListTropicanosComponent } from './list-tropicanos/list-tropicanos.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TropicanoComponent, TropicanoHomeComponent, ListTropicanosComponent],
+  imports: [RouterOutlet, TropicanoComponent, TropicanoHomeComponent, ListTropicanosComponent,MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
