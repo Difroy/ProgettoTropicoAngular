04c9cc2fbdec7baf3cd94483c8a1eeeed2a5c17a
Difroy <unifroy@gmail.com>
2024-09-26 15:40:48 +0200

    Update

--- src/app/model/Isle.ts
+++
@@ -1,7 +0,0 @@
-import { Building } from "./Building";
-
-export interface Isle {
-    id: number;
-    name: string;
-    buildings: Building[]; // Lista di edifici sull'isola
-}
--- src/app/model/Tropicano.ts
+++ src/app/model/Tropicano.ts
@@ -1,10 +1,19 @@
 export interface Tropicano {
+  
     id: number;
     name: string;
     surname: string;
     birthyear: number;
-    gender: "M" | "F";
-    education: "NONE" | "HS" | "COLLEGE";
-    party: "LEFT" | "RIGHT";
+    religion: 'NONE' | 'CTHULLHU' | 'TROPICANESIMO' | 'METALLARISMO';
+    gender: 'M' | 'F';
+    education: 'NONE' | 'HS' | 'COLLEGE';
+    party: 'LEFT' | 'RIGHT';
     satisfaction: number;
+    image:string;
+    residentialName?:string,
+    industryName?:string,
+    industryId?:number,
+    residentialId?:number
+
+
 }
